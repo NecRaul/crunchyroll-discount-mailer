da30973f911089d71cc7372588222ffa84759414
NecRaul <necraul@kuroneko.dev>
2023-10-10 23:18:43 +0400

    Initial commit github.com/NecRaul/rightstufanime-discount-mailer used as a base

---
+++ mail.py
@@ -0,0 +1,41 @@
+import re
+import smtplib
+import ssl
+import variables
+
+def mail_check(mail):
+    pattern = "([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\\.[A-Z|a-z]{2,})+"
+    return re.match(pattern, mail) is not None
+
+def email_validity():
+    sender_boolean = mail_check(variables.sender_email)
+    receiver_boolean = mail_check(variables.receiver_email)
+
+    if not sender_boolean and not receiver_boolean:
+        print("Sender email and receiver email are incorrect.")
+    elif not sender_boolean:
+        print("Sender email is incorrect.")
+    elif not receiver_boolean:
+        print("Receiver email is incorrect.")
+    else:
+        return True
+    
+def password_validity():
+    if (variables.password == ""):
+        print("You don't have a password.")
+    else:
+        return True
+    
+def send_mail(message):
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(
+        variables.smtp_server, variables.port, context=context
+    ) as server:
+        server.login(variables.sender_email, variables.password)
+        server.sendmail(variables.sender_email, variables.receiver_email, message)
+        
+email_check = email_validity()
+
+password_check = password_validity()
+
+send_check = False
