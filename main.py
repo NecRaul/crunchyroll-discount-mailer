7994d75ddb015fd676d6fe37fb9dd3bf5cb915e6
NecRaul <necraul@kuroneko.dev>
2023-11-06 15:51:05 +0400

    Deleted unnecessary if condition

--- main.py
+++ main.py
@@ -26,32 +26,31 @@ def main():
     if mail.email_check and mail.password_check:
         # setting up mail attributes for later
         message = MIMEMultipart("alternative")
-        message["Subject"] = variables.email_subject
+        message["Subject"] = variables.email_subjectsend_check
         message["From"] = variables.sender_email
         message["To"] = variables.receiver_email
 
         for key, value in variables.wishlists.items():
             url = f"https://store.crunchyroll.com/on/demandware.store/Sites-CrunchyrollUS-Site/en_US/Wishlist-ShowOthers?id={key}"
-            if url:
-                discounted_item_list = item_class.find_discounted_item(url, value)
+            discounted_item_list = item_class.find_discounted_item(url, value)
 
-                # if any item in any url is on sale, it will send you mail
-                if len(discounted_item_list) != 0:
-                    # use this if your wishlist consists of something other than books
-                    sorted_items = sorted(discounted_item_list, key=lambda x: x.name)
-                    # use this if your wishlist consists of books
-                    # sorted_items = sorted(discounted_item_list, key=custom_sort_key)
+            # if any item in any url is on sale, it will send you mail
+            if len(discounted_item_list) != 0:
+                # use this if your wishlist consists of something other than books
+                sorted_items = sorted(discounted_item_list, key=lambda x: x.name)
+                # use this if your wishlist consists of books
+                # sorted_items = sorted(discounted_item_list, key=custom_sort_key)
 
-                    message_parts = create_email_message(sorted_items)
+                message_parts = create_email_message(sorted_items)
 
-                    # turn these into plain/html MIMEText objects
-                    part1 = MIMEText(message_parts[0], "plain")
-                    part2 = MIMEText(message_parts[1], "html")
+                # turn these into plain/html MIMEText objects
+                part1 = MIMEText(message_parts[0], "plain")
+                part2 = MIMEText(message_parts[1], "html")
 
-                    # add HTML/plain-text parts to MIMEMultipart message
-                    # the email client will try to render the last part first
-                    message.attach(part1)
-                    message.attach(part2)
+                # add HTML/plain-text parts to MIMEMultipart message
+                # the email client will try to render the last part first
+                message.attach(part1)
+                message.attach(part2)
 
         if mail.send_check:
             mail.send_mail(message.as_string())
